7c11be750ef55a7d03f147b25c63d006e0b20757
Derek Dik <drd26@chomsky.cs.calvin.edu>
2015-12-18 13:12:06 -0500

    Added method documentation to main.py

--- app/Helpers/main.py
+++ app/Helpers/main.py
@@ -8,6 +8,15 @@ import mysql.connector
 import time
 from Scraper import Scraper
 
+'''
+toDict()
+
+Method to convert a model object for DNDi posts into a dictionary.
+
+Parameters: Article - Must be either ScientificArticle or PressRelease
+
+Returns: result dictionary
+'''
 def toDict(article):
     result = {}
     result[ 'id' ] = article._id
@@ -19,32 +28,63 @@ def toDict(article):
     result[ 'date' ] = article._datestr
     return result 
 
+'''
+DNDiDB
+
+Class models MySQL database connection
+'''
 class DNDiDB:
+    # Declarations
+    ###
+    # Tags provided by DNDi
     TAGS = [ 'HAT – Sleeping Sickness', 'Chagas disease', 'Filarial diseases', 'Hepatitis C', 'Leishmaniasis', 'Malaria', 'Mycetoma',
         'Paediatric HIV', 'Access', 'Advocacy', 'Funding', 'Partnership', 'Regulatory', 'Strengthening capacities', 'Treatment' ]
-    
+    # Valid month codes
     MONTH = { 'January': '01', 'February': '02', 'March': '03', 'April': '04','May': '05','June': '06',
             'July': '07','August': '08','September': '09','October': '10','November': '11','December': '12'}
+
+    '''
+    Parameters: List of database tables
+    '''
     def __init__(self, tables ):
         self.tables = tables
+        # Initialize a connection to the database
         self.connection = mysql.connector.connect(user='root', database='dndi', password='cs300')
+        # Create a new database curson
         self.cursor = self.connection.cursor()
 
+    '''
+    deleteALL()
+
+    Drop all data from the database
+    '''
     def deleteALL(self):
         for table in self.tables:
             self.cursor.execute( ( "delete from " + table ) )
         self.cursor.execute( ( "delete from tagPost" ) )
         self.connection.commit()
-        
+
+    '''
+    importTable
+
+    Import all relevant data into a given table.
+
+    Parameter: Table
+    '''
     def importTable( self, table ):
+        # Initialize SQL queries
         addStatment = ( "INSERT INTO " + table + " ( id, title, date, body, url, category ) VALUES( %s, %s, %s, %s, %s, %s )" )
         tagStatement = ( "INSERT INTO tagPost ( tag_id, post_id ) VALUES( %s, %s )" )
+        # Get the data for the given table
         data = self.getData( table )
-        
+        # If data is retrieved successfully
         if data is not False:
+            # For each row of the table
             for datum in data:
+                # Get the date
                 date = self._getDate( datum['date'] )
                 dataStatement = ( datum['id'],datum['title'],date,datum['body'],datum['url'],datum['category'])
+                # Apply tags
                 for tag in datum['tag']:
                     if tag[:3] == 'HAT':
                         tagID = 0
@@ -56,6 +96,7 @@ class DNDiDB:
                         if e.errno == 1406:
                             dataStatement = ( datum['id'],datum['title'][0:200], None, datum['body'],datum['url'],datum['category'])
                             self.cursor.execute( addStatment, dataStatement )
+                # Execute the query
                 try:
                     self.cursor.execute( addStatment, dataStatement )
                 except Exception as e:
@@ -68,7 +109,14 @@ class DNDiDB:
             self.connection.commit()
             
         print(table + ' is commited!')
-        
+
+    '''
+    getData()
+
+    Get all relevant data for the given table.
+
+    Parameter: Table
+    '''
     def getData( self, table ):
         if table == 'Event':
             return getAllEvent()
@@ -94,11 +142,26 @@ class DNDiDB:
             return result
         else:
             return False
-        
+
+    '''
+    importAll
+
+    Imports relevant data into each table specified.
+
+    No parameters
+    '''
     def importAll(self):
         for table in self.tables:
             self.importTable( table )
-            
+
+
+    '''
+    __getDate
+
+    Parse a date from a given string.
+
+    Parameter: String representing a date
+    '''
     def _getDate(self, string):
         string = string.strip()
         string = string.replace('[', '')
